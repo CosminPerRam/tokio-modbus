f71b2dd1ddebae6c6ba315dec36107cbdfda393e
CosminPerRam <cosmin.p@live.com>
2025-09-06 01:41:06 +0300

    attempt to reenable test

--- tests/rtu_all_exceptions.rs
+++ tests/rtu_all_exceptions.rs
@@ -11,26 +11,23 @@ use std::{thread, time::Duration};
 
 use exception::check_client_context;
 use tokio_modbus::{client, server::rtu::Server};
-use tokio_serial::SerialPortBuilder;
+use tokio_serial::SerialStream;
 
 use crate::exception::TestService;
 
 #[tokio::test]
-#[ignore = "we need to mock a serial port to test this"]
 async fn all_exceptions() -> Result<(), Box<dyn std::error::Error>> {
-    let builder = tokio_serial::new("/dev/ttyUSB0", 19200);
+    let (master, slave) = SerialStream::pair()?;
 
     tokio::select! {
-        _ = server_context(&builder) => unreachable!(),
-        _ = client_context(&builder) => (),
+        _ = server_context(master) => (),
+        _ = client_context(slave) => (),
     }
 
     Ok(())
 }
 
-async fn server_context(builder: &SerialPortBuilder) -> anyhow::Result<()> {
-    let server_serial = tokio_serial::SerialStream::open(builder).unwrap();
-
+async fn server_context(server_serial: SerialStream) -> anyhow::Result<()> {
     let _server = thread::spawn(move || {
         let rt = tokio::runtime::Runtime::new().unwrap();
         let server = Server::new(server_serial);
@@ -46,11 +43,9 @@ async fn server_context(builder: &SerialPortBuilder) -> anyhow::Result<()> {
 }
 
 // TODO: Update the `assert_eq` with a check on Exception once Client trait can return Exception
-async fn client_context(builder: &SerialPortBuilder) {
+async fn client_context(client_serial: SerialStream) {
     // Give the server some time for starting up
     tokio::time::sleep(Duration::from_millis(100)).await;
-    let client_serial = tokio_serial::SerialStream::open(builder).unwrap();
-
     let ctx = client::rtu::attach(client_serial);
 
     check_client_context(ctx).await;
